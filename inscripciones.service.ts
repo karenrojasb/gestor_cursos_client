c1bda6257cdc88de92c0db93c8aeaaba415ce7b6
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-06-05 13:42:29 -0500

    Update inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -22,3 +22,42 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
     ...nombreCampoExtra,
   }));
 };
+<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+  <div>
+    <label>Profesor</label>
+    <select
+      name="Profesor"
+      value={formData.Profesor ?? ""}
+      onChange={handleChange}
+      className="w-full border p-2 rounded-lg"
+    >
+      <option value="">
+        {formData.Profesor || "Selecciona una opción"}
+      </option>
+      {profesores.map((p) => (
+        <option key={p.id_emp} value={p.id_emp}>
+          {p.nombre}
+        </option>
+      ))}
+    </select>
+  </div>
+
+  <div>
+    <label>Segundo Profesor</label>
+    <select
+      name="SegundoPro"
+      value={formData.SegundoPro ?? ""}
+      onChange={handleChange}
+      className="w-full border p-2 rounded-lg"
+    >
+      <option value="">
+        {formData.SegundoPro || "Selecciona una opción"}
+      </option>
+      {profesores.map((p) => (
+        <option key={p.id_emp} value={p.id_emp}>
+          {p.nombre}
+        </option>
+      ))}
+    </select>
+  </div>
+</div>
