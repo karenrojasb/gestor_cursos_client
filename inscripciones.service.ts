194d2e8a703bf0bce22a402a941202bea71d831e
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-06-05 11:22:09 -0500

    Actualizar inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -1,21 +1,28 @@
+type Profesor = {
+  id_emp: string;
+  nombre: string;
+  publico: number;
+};
+
 useEffect(() => {
   async function fetchProfesores() {
     try {
       const response = await fetch("http://localhost:8090/api/cursos/profesores");
       if (!response.ok) throw new Error("Error al obtener los profesores");
 
-      const data = await response.json();
+      const data: Profesor[] = await response.json();
 
-      const profesoresPublicos = data.filter((prof: { publico: number }) => prof.publico === 1);
+      const profesoresPublicos = data.filter((prof: Profesor) => prof.publico === 1);
 
       const profesorId = formData.Profesor ? formData.Profesor.toString() : "";
-      const profesorCurso = profesoresPublicos.find(p => p.id_emp.toString() === profesorId);
+      const profesorCurso = profesoresPublicos.find((p: Profesor) => p.id_emp.toString() === profesorId);
 
       if (!profesorCurso && profesorId) {
-        const profesorEncontrado = data.find((p: any) => p.id_emp.toString() === profesorId);
+        const profesorEncontrado = data.find((p: Profesor) => p.id_emp.toString() === profesorId);
         profesoresPublicos.push({
           id_emp: profesorId,
           nombre: profesorEncontrado ? profesorEncontrado.nombre : profesorId,
+          publico: 0, // o el valor que tenga sentido
         });
       }
 
