538f145afe42ee6d362fa26c20641a05a8a635df
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-10-31 11:20:08 -0500

    Update inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -1,6 +1,28 @@
+import { DateTime } from 'luxon';
+
 export const getNowDate = (): Date => {
     return DateTime.now()
         .setZone('America/Bogota')
         .minus({ hours: 5 })
         .toJSDate();
+};
+
+/**
+ * Formats a JavaScript Date object to a string in 'yyyy-mm-dd' format
+ * @param date - The Date object to format
+ * @returns A string in 'yyyy-mm-dd' format, or an empty string if date is invalid
+ */
+export const formatDateToYMD = (): string => {
+    const date = DateTime.now()
+        .setZone('America/Bogota')
+        .minus({ hours: 5 })
+        .toJSDate();
+
+    return DateTime.fromJSDate(date).toISODate() || '';
+};
+
+export const formatoDateToDDMMYYYY = (): string => {
+  return DateTime.now()
+    .setZone('America/Bogota')
+    .toFormat('yyyyLLdd');
 };
