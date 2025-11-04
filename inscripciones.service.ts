bee23b6803ad30d68b78325533267836de749097
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-11-04 08:45:40 -0500

    Update inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -1,28 +1,42 @@
-import { DateTime } from 'luxon';
+import { IsNumber, IsOptional, IsString, Length, IsInt } from 'class-validator';
+import { Transform, Type } from "class-transformer";
 
-export const getNowDate = (): Date => {
-    return DateTime.now()
-        .setZone('America/Bogota')
-        .minus({ hours: 5 })
-        .toJSDate();
-};
+export class ArticuloDto {
+    @IsString({message: 'El campo item debe ser una cadena de texto'})
+    @Length(1, 40, {message: 'El campo item debe tener entre 1 y 40 caracteres'})
+    item: string;
 
-/**
- * Formats a JavaScript Date object to a string in 'yyyy-mm-dd' format
- * @param date - The Date object to format
- * @returns A string in 'yyyy-mm-dd' format, or an empty string if date is invalid
- */
-export const formatDateToYMD = (): string => {
-    const date = DateTime.now()
-        .setZone('America/Bogota')
-        .minus({ hours: 5 })
-        .toJSDate();
+    @Transform(({ value}) => parseFloat(value))
+    @IsNumber({ maxDecimalPlaces: 4 }, { message: 'La cantidad debe tener máximo 4 decimales' })
+    cantidad: number;
 
-    return DateTime.fromJSDate(date).toISODate() || '';
-};
 
-export const formatoDateToDDMMYYYY = (): string => {
-  return DateTime.now()
-    .setZone('America/Bogota')
-    .toFormat('yyyyLLdd');
-};
+    @Transform(({ value}) => parseFloat(value))
+    @IsNumber({ maxDecimalPlaces: 4 }, { message: 'El cos_uni debe tener máximo 4 decimales' })
+    cos_uni: number;
+
+    @IsNumber({ maxDecimalPlaces: 4 }, { message: 'El pre_vta debe tener máximo 4 decimales'})
+    pre_vta: number;
+
+    @IsOptional()
+    @IsNumber({ maxDecimalPlaces: 4 })
+    cos_unai?: number;
+
+    @IsNumber({ maxDecimalPlaces: 4 }, { message: 'La por_des debe tener máximo 4 decimales'})
+    por_des: number;
+
+    @IsOptional()
+    @IsString({message: 'El usr_descrip_cue debe ser una cadena de texto'})
+    @Length(1, 200, { message: 'El campo num_fact debe tener entre 1 y 14 caracteres' })
+    usr_descrip_cue?: string;
+
+    @IsOptional()
+    @IsString()
+    @Transform(({ value }) => value ?? null)
+    bodega?: string | null;
+
+    @IsOptional()
+    @IsNumber({})
+    dia_pla: number;
+
+}
