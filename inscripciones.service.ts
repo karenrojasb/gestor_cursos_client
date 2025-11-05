005072e4e89c7b9dad0ea8aad08f7c9aebe48e6c
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-11-05 09:08:04 -0500

    Update inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -1,10 +1,21 @@
-await this.prismaService.$queryRawUnsafe<any[]>(`
-    EXEC dbo.USR_sp_gen_afeinv_preciso 
-        @ano_doc = ${getNowDate().getFullYear()}, 
-        @per_doc = '${getCurrentMonth()}', 
-        @sub_tip = '${data.sub_tip}', 
-        @tip_doc = '${tip_doc}', 
-        @num_doc = ${num_doc}
-`);
+USE [Novasoft]
+GO
 
-retu
+DECLARE	@return_value int
+
+EXEC	@return_value = [dbo].[USR_sp_gen_afeinv_preciso]
+		@iano_doc = N'2025',
+		@fano_doc = N'2025',
+		@iper_doc = N'11',
+		@fper_doc = N'11',
+		@isub_tip = N'C007',
+		@fsub_tip = N'C007',
+		@inum_doc = N'55946',
+		@fnum_doc = N'55946',
+		@ifec_doc = N'2025-11-04',
+		@ffec_doc = N'2025-11-04',
+		@indver = false
+
+SELECT	'Return Value' = @return_value
+
+GO
