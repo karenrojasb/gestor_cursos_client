4b41409298bea79df28127627bc5f57496f8c2c4
karenrojasb <karen.rojas-b@escuelaing.edu.co>
2025-11-11 08:33:03 -0500

    Actualizar inscripciones.service.ts

--- inscripciones.service.ts
+++ inscripciones.service.ts
@@ -1,5 +1,9 @@
-SELECT 
-    fk.name AS ForeignKey,
+SELECT  
+    fk.name AS NombreRelacion,
+    CASE 
+        WHEN tp.name = 'inv_inf_inv' THEN 'Es clave foránea en otra tabla (inv_inf_inv es la tabla padre)'
+        WHEN tr.name = 'inv_inf_inv' THEN 'Es clave foránea en inv_inf_inv (inv_inf_inv es la tabla hija)'
+    END AS TipoRelacion,
     tp.name AS TablaPadre,
     cp.name AS ColumnaPadre,
     tr.name AS TablaRelacionada,
@@ -11,4 +15,4 @@ INNER JOIN sys.columns AS cp ON fkc.parent_object_id = cp.object_id AND fkc.pare
 INNER JOIN sys.tables AS tr ON fkc.referenced_object_id = tr.object_id
 INNER JOIN sys.columns AS cr ON fkc.referenced_object_id = cr.object_id AND fkc.referenced_column_id = cr.column_id
 WHERE tp.name = 'inv_inf_inv' OR tr.name = 'inv_inf_inv'
-ORDER BY TablaPadre, TablaRelacionada;
+ORDER BY TablaPadre, TablaRelacionada, ColumnaRelacionada;
